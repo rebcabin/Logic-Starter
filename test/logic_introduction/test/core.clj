35f9e259e2fa3c5ab69f4e6644d40c9f4cd55bb1
Reb <rebcabin@Rebs-MacBook-Pro.local>
2013-07-10 19:05:09 -0700

    add more typedo

--- test/logic_introduction/test/core.clj
+++ test/logic_introduction/test/core.clj
@@ -4,13 +4,28 @@
   (:use     [clojure.core.logic]
             [logic-introduction.core]))
 
-(test/deftest first-type-test
+(test/deftest first-type-tests
   (test/is (= '(_0)
               (run* [q]
-                    (typedo [ ['f :- [Integer :> Integer] ] ;; 'f is of type Integer -> Integer
-                              ['g :- Integer] ;; 'g is of type Integer
-                              ] 
+                    (typedo [['f :- [Integer :> Integer] ] ;; 'f is of type Integer -> Integer
+                             ['g :- Integer] ;; 'g is of type Integer
+                             ] 
                             [:apply 'f 'g] ;; Determine the resulting type of ('f 'g) ..
                             Integer) ;;  and succeed if it is Integer
-                    ))))
+                    )))
+  (test/is (= ()
+              (run* [q]
+                    (typedo [['f :- [Float :> Integer] ]
+                             ['g :- Integer]
+                             ] 
+                            [:apply 'f 'g]
+                            Integer) ;; expect fail
+                    )))
+  #_(test/is (= ()
+              (run* [q]
+                    (typedo [['f :- [Float :> Integer]]
+                             ['g :- Float]
+                             ['h :- [Integer :> Float]]]
+                            Float))))
+  )
 
